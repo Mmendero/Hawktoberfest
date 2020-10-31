03f3f8580bb0606b2be02c2ac451b814b78db729
Teresa M <teresawithoutah@gmail.com>
2020-10-30 22:19:50 -0400

    Create running.js

---
+++ running.js
@@ -0,0 +1,29 @@
+//ARRAY OF CENSOR WORDS
+var spoilers = ["today", "day", "run"];
+
+
+
+// Checking page title
+if (document.title.indexOf("Tumblr") != -1) {
+//if on tumblr print this
+console.log("ur on tumblr");
+
+//loop for searching page for elements in array
+var found = spoilers.every(function(word) {
+    return document.documentElement.innerText.indexOf(word) > -1;
+});
+
+//if exists print this
+if (found) { 
+	//ARRAY OF CENSOR WORDS
+var spoilers = ["today", "day", "run"];
+ 
+console.log("spoilers found");
+$("p:contains("+$spoilers+")").css("background-color", "yellow");
+ 
+
+}
+
+}else{
+	console.log("ur not on tumblr");
+}
